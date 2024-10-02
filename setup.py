4ab387f4c9474715808eb8b078b0e0a2bb1fa3a9
mohamedsafy <85454104+mohamedsafy@users.noreply.github.com>
2024-10-02 19:09:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,1 @@
-from setuptools import setup
 
-APP = ['main.py']  # Replace with your script name
-DATA_FILES = []  # List additional files you want to include
-OPTIONS = {
-    'argv_emulation': True
-}
-
-setup(
-    app=APP,
-    data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
-)
