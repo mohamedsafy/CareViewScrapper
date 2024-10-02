ffbf5654eb081277a4ba854871fddd7b0ce60b09
mohamedsafy <85454104+mohamedsafy@users.noreply.github.com>
2024-10-02 18:28:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 APP = ['main.py']  # Replace with your script name
 DATA_FILES = []  # List additional files you want to include
 OPTIONS = {
-    'argv_emulation': True,
-    'packages': ['beautifulsoup4','pandas', 'selenium', 'webdriver_manager'],  # List your required packages here
+    'argv_emulation': True
 }
 
 setup(
